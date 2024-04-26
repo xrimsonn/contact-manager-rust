bab072a89f17b79f8e607316c54b06372548824d
José Antonio Rosales <jose.rosales.mald@gmail.com>
2024-04-25 18:07:26 -0600

    updated run_menu with json functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,33 @@
 use crate::contact::*;
+use crate::json::*;
 use crate::Contact;
 use std::io::{self, Write};
 use std::process::Command;
+
+pub fn run_menu(contacts: &mut Vec<Contact>) {
+    load_contacts_json(contacts).unwrap();
+    loop {
+        println!("-------Menu-------");
+        println!("1. Contact list");
+        println!("2. Add contact");
+        println!("3. Search contact");
+        println!("4. Delete contact");
+        println!("0. Exit");
+        let opt = read_num_input("Enter an option");
+        clear_terminal();
+
+        match opt {
+            1 => print_contact_list(contacts),
+            2 => add_contact(contacts),
+            3 => search_contact(contacts),
+            4 => delete_contact(contacts),
+            0 | _ => {
+                save_contacts_json(contacts).unwrap();
+                std::process::exit(0)
+            }
+        };
+    }
+}
 
 fn clear_terminal() {
     let _ = Command::new("clear").status();
@@ -27,25 +53,4 @@ pub fn read_string_input(msg: &str) -> String {
         .read_line(&mut var)
         .expect("Failed to read line.");
     var.trim().to_string()
-}
-
-pub fn run_menu(contacts: &mut Vec<Contact>) {
-    loop {
-        println!("-------Menu-------");
-        println!("1. Contact list");
-        println!("2. Add contact");
-        println!("3. Search contact");
-        println!("4. Delete contact");
-        println!("0. Exit");
-        let opt = read_num_input("Enter an option");
-        clear_terminal();
-
-        match opt {
-            1 => print_contact_list(contacts),
-            2 => add_contact(contacts),
-            3 => search_contact(contacts),
-            4 => delete_contact(contacts),
-            0 | _ => std::process::exit(0),
-        };
-    }
 }
