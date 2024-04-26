8b1e093a5c81a1259bbc10817b20987f42523f08
José Antonio Rosales <jose.rosales.mald@gmail.com>
2024-04-25 18:07:05 -0600

    cleaned main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
 mod contact;
+mod json;
 mod utils;
 use contact::*;
 use utils::run_menu;
 
 fn main() {
-    let antonio = Contact {
-        name: String::from("Antonio Rosales"),
-        phone_number: String::from("614-607-5690"),
-        address: String::from("Mina el Potosi 125"),
-    };
-
-    let esau = Contact {
-        name: String::from("Esau Melendez"),
-        phone_number: String::from("614-255-2356"),
-        address: String::from("Mina Portillo 1830"),
-    };
-
     let mut contacts: Vec<Contact> = Vec::new();
-    contacts.push(antonio);
-    contacts.push(esau);
-
     run_menu(&mut contacts);
 }
